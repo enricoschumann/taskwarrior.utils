4a69c8978a8fe1fb116ef7e3f975b3efe4cda2c5
Enrico Schumann <es@enricoschumann.net>
2020-01-02 11:06:23 +0100

    Use colnames in as.data.frame method

--- R/read_tasks.R
+++ R/read_tasks.R
@@ -16,12 +16,12 @@ function(x, row.names = NULL, optional = FALSE, ...) {
             tmp
     }
 
-    res <- list(sapply(x, fetch_field, "uuid"),
-                sapply(x, fetch_field, "id"),
-                sapply(x, fetch_field, "description"),
-                c(as.POSIXct(sapply(x, fetch_field, "entry"), tz = "UTC", format = "%Y%m%dT%H%M%SZ")),
-                c(as.POSIXct(sapply(x, fetch_field, "due"), tz = "UTC", format = "%Y%m%dT%H%M%SZ")),
-                sapply(x, fetch_field, "status")
+    res <- list(uuid = sapply(x, fetch_field, "uuid"),
+                id = sapply(x, fetch_field, "id"),
+                description = sapply(x, fetch_field, "description"),
+                entry = c(as.POSIXct(sapply(x, fetch_field, "entry"), tz = "UTC", format = "%Y%m%dT%H%M%SZ")),
+                due = c(as.POSIXct(sapply(x, fetch_field, "due"), tz = "UTC", format = "%Y%m%dT%H%M%SZ")),
+                status = sapply(x, fetch_field, "status")
                 )
     attr(res, "row.names") <- .set_row_names(length(res[[1L]]))
     class(res) <- "data.frame"
