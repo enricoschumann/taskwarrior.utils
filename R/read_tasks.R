d09fde30c618e7b9e83c3f1fbf065c89cf3c7506
Enrico Schumann <es@enricoschumann.net>
2024-11-05 16:49:49 +0100

    Allow selection of columns
    The as.data.frame method now supports the selection of
    fields to include.  Fields can also be coerced to POSIXct.

--- R/read_tasks.R
+++ R/read_tasks.R
@@ -6,7 +6,11 @@ function(file, ...) {
 }
 
 as.data.frame.taskwarrior_tasks <-
-function(x, row.names = NULL, optional = FALSE, ...) {
+function(x, row.names = NULL, optional = FALSE, ...,
+         fields = c("uuid", "id", "description",
+                    "entry", "due", "status"),
+         time.fields = c("due", "end", "entry", "modified")) {
+
 
     fetch_field <- function(list, field) {
         tmp <- list[[field]]
@@ -16,13 +20,19 @@ function(x, row.names = NULL, optional = FALSE, ...) {
             tmp
     }
 
-    res <- list(uuid = sapply(x, fetch_field, "uuid"),
-                id = sapply(x, fetch_field, "id"),
-                description = sapply(x, fetch_field, "description"),
-                entry = c(as.POSIXct(sapply(x, fetch_field, "entry"), tz = "UTC", format = "%Y%m%dT%H%M%SZ")),
-                due = c(as.POSIXct(sapply(x, fetch_field, "due"), tz = "UTC", format = "%Y%m%dT%H%M%SZ")),
-                status = sapply(x, fetch_field, "status")
-                )
+    res <- list()
+    for (field in fields) {
+        res[[field]] <-
+            if (field %in% time.fields) {
+                c(as.POSIXct(sapply(x, fetch_field, field),
+                             tz = "UTC",
+                             format = "%Y%m%dT%H%M%SZ"))
+        } else {
+            sapply(x, fetch_field, field)
+        }
+
+    }
+
     attr(res, "row.names") <- .set_row_names(length(res[[1L]]))
     class(res) <- "data.frame"
     res
